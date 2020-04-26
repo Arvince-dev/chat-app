0c0b013ebf94f0804275708324fc1d8af0ab89b2
vince <arvincedev@gmail.com>
2020-04-27 01:26:01 +0800

    index changes

--- index.js
+++ index.js
@@ -7,8 +7,12 @@ app.get('/', (req, res) => {
 });
 
 
-http.listen(3000, () => {
-    console.log('Listening on port *: 3000');
+// http.listen(3000, () => {
+//     console.log('Listening on port *: 3000');
+// });
+
+http.listen(process.env.PORT || 3000, function(){
+    console.log('listening on', http.address().port);
 });
 
 
